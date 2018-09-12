93b7887d84811b5b5740abd868086feb39b61b10
dojo-json <jfranz@codingdojo.com>
2018-09-11 19:46:17 -0700

    modularize with input/output

--- books/src/app/app.component.ts
+++ books/src/app/app.component.ts
@@ -1,40 +1,8 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
-import { Book } from './models/book';
-import { BOOKS } from './data/book-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  book = new Book();
-  books: Book[] = BOOKS;
-  selectedBook: Book;
-
-  onSubmit(event: Event, form: NgForm) {
-    event.preventDefault();
-
-    console.log('submitting form', this.book);
-
-    this.books.push(this.book);
-
-    this.book = new Book();
-
-    console.log('books array', this.books);
-
-    form.reset();
-  }
-
-  onSelect(book: Book) {
-    console.log('selecting ', book);
-
-    if (this.selectedBook === book) {
-      this.selectedBook = null;
-    } else {
-      this.selectedBook = book;
-    }
-  }
-}
+export class AppComponent {}
---
+++ books/src/app/books/book-list/book-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Book } from '../../models/book';
+import { BOOKS } from '../../data/book-data';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css'],
+})
+export class BookListComponent implements OnInit {
+  books: Book[] = BOOKS;
+  selectedBook: Book;
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSelect(book: Book) {
+    console.log('selecting ', book);
+
+    this.selectedBook = this.selectedBook === book ? null : book;
+
+    // if (this.selectedBook === book) {
+    //   this.selectedBook = null;
+    // } else {
+    //   this.selectedBook = book;
+    // }
+  }
+
+  onCreate(book: Book) {
+    console.log('creating book', book);
+
+    this.books.push(book);
+  }
+}
